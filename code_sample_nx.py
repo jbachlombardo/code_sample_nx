052e326f4504d01deecd058cf78480f21591ae6f
jbachlombardo <jbachlombardo@gmail.com>
2018-05-15 14:02:27 -0400

    Update code_sample_nx.py

--- code_sample_nx.py
+++ code_sample_nx.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 df_plot = pd.read_csv('filepath') #csv generated from separate program that uses beautifulsoup to read through online activity
 
+#CREATE GRAPH AND CALCULATE CENTRALITIES / GENERATE LABELS FOR TOP ENGAGEMENT TARGETS
 plt.figure(figsize = (10, 8))
 G = nx.from_pandas_dataframe(df_plot, 'From', 'To', 'Order') #'Order' described type of connection (not visualized but used separately to inform engagement)
 centrality = nx.degree_centrality(G)
@@ -19,10 +20,11 @@ for i, node in enumerate(G.nodes()) : #Generate labels the graphic with numbers 
         labels[node] = node
 nx.draw(G, pos = pos, with_labels = False, node_color = 'black', alpha = 0.25, node_size = 3, linewidths = 0.75)
 nx.draw_networkx_labels(G, pos, labels, font_size = 8, font_color = 'red', font_weight = 'bold')
+plt.show()
+
+#PRINT LABELS CORRESPONDING TO GRAPH
 for k, v in labels.items() : #Print the number and corresponding name for each hub for reference (minus the hubs we already have identified)
     if k in centers :
         continue
     else :
         print (v, '-', k)
-plt.title('Digital network')
-plt.show()
